5d63d6a99292f8f4806d56917e211ba6d09e51de
Sandra Fernandz <sandraenmunich@gmail.com>
2022-04-24 10:48:51 +0200

    added images and components

---
+++ react-slider/src/components/SliderData.js
@@ -0,0 +1,23 @@
+export const SliderData = [
+  {
+    image:
+      'https://images.unsplash.com/photo-1517495306984-f84210f9daa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2t5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1508020963102-c6c723be5764?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8c2t5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+  },
+
+  {
+    image:
+      'https://images.unsplash.com/photo-1542272201-b1ca555f8505?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c2t5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1514477917009-389c76a86b68?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHNreXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1428908728789-d2de25dbd4e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHNreXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60',
+  },
+];
