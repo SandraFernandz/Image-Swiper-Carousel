3309f4a90476597f7504c1c467c79eeef419c387
Sandra Fernandz <sandraenmunich@gmail.com>
2022-04-24 12:10:05 +0200

    added arrows, handle events functions, states

--- react-slider/src/components/ImageSlider.js
+++ react-slider/src/components/ImageSlider.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import { useState } from 'react';
 import { SliderData } from './SliderData';
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
 
-function ImageSlider() {
+function ImageSlider({ slides }) {
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current - 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  if (!Array.isArray(slides) || slides.length <= 0) {
+    return null;
+  }
+
   return (
-    <>
-      {SliderData.map((slide, index) => {
-        return <img src={slide.image} alt="sky" key={index} />;
+    <section className="slider">
+      <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />
+      <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide} />
+      {SliderData.map((slide, index, height) => {
+        return (
+          <img src={slide.image} alt="sky" key={index} className="image" />
+        );
       })}
-    </>
+    </section>
   );
 }
 
